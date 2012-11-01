81377638d4f05a7931f0c1be1c2fbe751578bc47
Alvaro Cabrera <alvaro@outofthebox.mx>
2012-11-01 15:26:07 -0600

    Remove invalid chars; fix

--- lib/Servant/Helpers.php
+++ lib/Servant/Helpers.php
@@ -38,7 +38,7 @@ class Helpers
 
   public static function underscore($test)
   {
-    $test = preg_replace('/[A-Z](?=\w)/', '_\\0', $test);
+    $test = preg_replace('/\W/', '', preg_replace('/[A-Z](?=\w)/', '_\\0', $test));
     $test = preg_replace_callback('/(^|\W)([A-Z])/', function ($match) {
       "$match[1]_" . strtolower($match[2]);
     }, $test);
