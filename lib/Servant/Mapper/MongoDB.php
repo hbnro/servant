e74ba98cd045d5720205942c59eeb7f50130fe90
Alvaro Cabrera <alvaro@outofthebox.mx>
2013-02-12 14:00:28 -0600

    Skip non-string values; fix

--- lib/Servant/Mapper/MongoDB.php
+++ lib/Servant/Mapper/MongoDB.php
@@ -272,6 +272,10 @@ class MongoDB extends \Servant\Base
 
   protected static function field($value)
   { // TODO: there is a better way?
+    if ( ! is_string($value)) {
+      return $value;
+    }
+
     if (preg_match('/^\d{4}\D\d{2}\D\d{2}(?:\D?\d{2}:\d{2}(?::\d{2})?)?$/', $value)) {
       return new \MongoDate(strtotime($value));
     }
